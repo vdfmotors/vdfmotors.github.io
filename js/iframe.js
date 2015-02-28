f99d0b55e69883f902a251bd0e29e59b4fcd96be
rolandvdf <Roland@MacBook-Pro-van-Roland.local>
2015-02-28 14:26:30 +0100

    test

--- js/iframe.js
+++ js/iframe.js
@@ -1,5 +1,16 @@
-
-            function autoResize(iframe) {
-                $(iframe).height($(iframe).contents().find('html').height()-90);
-                iframe.contentWindow.scrollTo(0,35);
-            }
+function(run){
+for (i=0;i<frames.length; i++) {
+var f1 = document.getElementsByTagName('iframe')[i];
+if(!f1 && window.addEventListener && !run){
+document.addEventListener('DOMContentLoaded', arguments.callee, false);
+return;
+}
+if(!f1){setTimeout(arguments.callee, 300); return;}
+f2 = f1.cloneNode(false);
+f1.src = 'about: blank';
+f2.frameBorder = '0';
+f2.allowTransparency = 'yes';
+f2.scrolling = 'no';
+f1.parentNode.replaceChild(f2, f1);
+}
+})();
